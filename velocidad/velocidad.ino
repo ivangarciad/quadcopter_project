2ef1bd2fec1d94c58dbe991db1e4d796ed865598
igdaza <igdaza@igdaza-desktop.(none)>
2014-08-02 17:09:25 +0200

    First version with motors running.

--- velocidad/velocidad.ino
+++ velocidad/velocidad.ino
@@ -1,79 +1,39 @@
-// this uses the Arduino servo library included with version 0012 
-
-// caution, this code sweeps the motor up to maximum speed !
-// make sure the motor is mounted securily before running.
-
 #include <Servo.h> 
 
-Servo myservo;
-byte recibiendoByte;
+Servo myservo; 
+int pwm_value = 32;
 
-int time_1 = 0;
-int time_2 = 0;
-int T = 100;
-int delay_value = 0;
-int angle = 0;
-static int speed=335;
+void setup() 
+{ 
+	Serial.begin(9600);
+  Serial.println("initializing");
 
-void arm(){
-  // arm the speed controller, modify as necessary for your ESC  
-  setSpeed(0); 
-  delay(1000); //delay 1 second,  some speed controllers may need longer
-}
-
-
-void setup()
-{
-  myservo.attach(9);
-  Serial.begin(9600);
-  //arm();  
-}
-
-void setSpeed(int speed){
-  // speed is from 0 to 100 where 0 is off and 100 is maximum speed
-  // the following maps speed values of 0-100 to angles from 0-180,
-  // some speed controllers may need different values, see the ESC instructions
-  angle = map(speed, 0, 1023, 0, 179);
-  Serial.println (speed);
-  Serial.println (angle);
-  myservo.write(angle);    
-}
-
-void loop()
-{
-  time_1 = millis();
-  setSpeed(OrdenSubirBajar());
-  time_2 = millis();
-
-  
-  delay_value = T - (time_2 - time_1);
-  delay(delay_value);
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object 
+  delay(4000);
+	
+  myservo.write(0);
+  Serial.println("listo");
 } 
 
-int OrdenSubirBajar()
-{
-
-  if (Serial.available() > 0) 
+void loop() 
+{ 
+	if(Serial.available() > 0)
   {
-     recibiendoByte = Serial.read(); // Leemos el Byte recibido
-     //Serial.println(recibiendoByte);
-     
-     if (recibiendoByte == 65 || recibiendoByte == 97)
-     {  // A o a
-        Serial.println("SUBIR");
-        speed += 1;
-     }
-    if (recibiendoByte == 90 || recibiendoByte ==122) 
-    { // Z o z
-      Serial.println("BAJAR");
-      speed -= 1;
+    // read the value
+    char ch = Serial.read();
+
+    if (ch == 'u' || ch == 'U')
+    {
+      pwm_value++;
+      Serial.println(pwm_value);
+			myservo.write(pwm_value);
     }
-    if (recibiendoByte == 83  || recibiendoByte == 115)
-    { // T o t
-      Serial.println("Stop!!");
-      speed = 35;
+    if (ch == 'd' || ch == 'D')
+    {
+      pwm_value--;
+      Serial.println(pwm_value);
+			myservo.write(pwm_value);
     }
   }
-  return (speed);
-}
 
+}
